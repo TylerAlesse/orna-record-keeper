fec4092fb019b2f6d8dfc22cf5f1263c6bc49f18
TylerAlesse <TylerAlesse@gmail.com>
2026-01-29 11:28:33 -0600

    CodexData passing

--- DBManager/tests/test_CodexData.py
+++ DBManager/tests/test_CodexData.py
@@ -6,16 +6,16 @@ from classes.DBCodexData import DBCodexData
 def mock_DBCodexData() -> DBCodexData:
     return DBCodexData(2, "Monster", 1, "Bandit", None, "Complete", 1, 5)
 
-def test_DBCodexData_init(mock_CodexData: DBCodexData) -> None:
-    assert mock_CodexData.ID == 2
-    assert mock_CodexData.type == "Monster"
-    assert mock_CodexData.name == "Bandit"
-    assert mock_CodexData.event == None
-    assert mock_CodexData.status == "Complete"
-    assert mock_CodexData.manifested == 1
-    assert mock_CodexData.kills == 5
+def test_DBCodexData_init(mock_DBCodexData: DBCodexData) -> None:
+    assert mock_DBCodexData.ID == 2
+    assert mock_DBCodexData.type == "Monster"
+    assert mock_DBCodexData.name == "Bandit"
+    assert mock_DBCodexData.event == None
+    assert mock_DBCodexData.status == "Complete"
+    assert mock_DBCodexData.manifested == 1
+    assert mock_DBCodexData.kills == 5
 
-def test_DBCodexData_exportJSON(mock_CodexData: DBCodexData) -> None:
+def test_DBCodexData_exportJSON(mock_DBCodexData: DBCodexData) -> None:
     expected = '{ID:2,Type:"Monster",Tier:1,Name:"Bandit",Event:null,Status:"Complete",Manifested:1,Kills:5}'
-    actual = mock_CodexData.exportJSON()
+    actual = mock_DBCodexData.exportJSON()
     assert actual == expected
